e694392eb4cdad5d019c3abed099423664dba282
TylerPete <tpeterson4@byupathway.edu>
2026-05-16 18:18:07 -0500

    did more stuff

---
+++ routes/data/database.js
@@ -0,0 +1,33 @@
+const dotenv = require("dotenv");
+dotenv.config();
+
+const MongoClient = require("mongo").MongoClient;
+
+let database;
+
+const initDb = (callback) => {
+    if (_db) {
+        console.log("Db is already initialized");
+        return callback(null, _db);
+    }
+    MongoClient.connect(process.env.MONGODB_URL)
+    .then((client) => {
+        _db = client;
+        callback(null, _db);
+    })
+    .catch((err) => {
+        callback(err);
+    });
+};
+
+const getDatabase = () => {
+    if (!database) {
+        throw Error("Database not initialized")
+    }
+    return database;
+};
+
+module.exports = {
+    initDb,
+    getDatabase
+};
--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const express = require("express");
+
+const mongodb = require("./data/database");
 const app = express();
 
 const port = process.env.PORT || 3000;
@@ -6,4 +8,10 @@ const port = process.env.PORT || 3000;
 
 app.use("/", require("./routes"));
 
-app.listen(port, () => console.log(`Running on port ${port}`));
+mongodb.initDb((err) => {
+    if (err) {
+        console.log(err);
+    } else {
+        app.listen(port, () => console.log(`Running on port ${port}`));
+    }
+})
